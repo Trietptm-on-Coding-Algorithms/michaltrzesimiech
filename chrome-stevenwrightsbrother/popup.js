66b0242e0f5df0959cb50068eee56f8d90ece042
michaltrzesimiech <michal.trzesimiech@gmail.com>
2016-04-13 18:51:21 +0200

    Added files via upload

--- chrome-stevenwrightsbrother/popup.js
+++ chrome-stevenwrightsbrother/popup.js
@@ -1,11 +1,17 @@
-//$(document).ready(function() {
+$(document).ready(function() {
 
-    var items = Array('go west', 'go east', 'go north', 'go upwards', 'go downwards', 'go within');
+    var items = Array(
+	'You can\'t have everything. Where would you put it?',
+	'I went to a restaurant that serves \"breakfast at any time\". So I ordered French Toast during the Renaissance.',
+	'What\'s another word for Thesaurus?',
+	'I intend to live forever. So far, so good.',
+	'I couldn\'t repair your brakes, so I made your horn louder.',
+	'Curiosity killed the cat. But for a while I was a suspect.');
     var item = items[Math.floor(Math.random() * items.length)];
 
     $('#show').text(item);
 
-//});
+});
 //});
 
 //$(document).ready(function() {  
